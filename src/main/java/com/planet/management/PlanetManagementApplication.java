44b42a6cdb4c2fed754302f50735eeaca533c43d
Dani <dani>
2021-06-06 18:28:49 +0600

    project initialization

---
+++ src/main/java/com/planet/management/PlanetManagementApplication.java
@@ -0,0 +1,13 @@
+package com.planet.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanetManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlanetManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/planet/management/PlanetManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planet.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanetManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
